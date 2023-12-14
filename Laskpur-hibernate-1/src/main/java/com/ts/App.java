756f460d814eb815466d844b320b1ff9ab5a5c50
Vinodkumar Borse <borse002@gmail.com>
2023-12-14 09:41:44 +0530

    Add files via upload

--- Laskpur-hibernate-1/src/main/java/com/ts/App.java
+++ Laskpur-hibernate-1/src/main/java/com/ts/App.java
@@ -1,5 +1,7 @@
 package com.ts;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,6 +22,24 @@ public class App
     	session.beginTransaction();
     	//----
     		
+    		Student s1 = new Student();
+    		s1.setName("Bholu");
+    		s1.setAddress("Dholak pur");
+    		s1.setDob(new Date());
+    		session.save(s1);
+    		
+    		Student s2 = new Student("SRK", "Mumbai", new Date());
+    		session.save(s2);
+    		Student s3 = new Student("Vinod", "Pune", new Date());
+    		session.save(s3);
+    		
+//    		Student s = new Student();
+//   		s.setRollno(6);
+// 		    session.delete(s);
+    	
+//    	Student s = session.get(Student.class, 2);
+//    	System.out.println(s.getName());
+    	
     	//----
     	session.getTransaction().commit();
     	session.close();
--- Laskpur-hibernate-1/src/main/java/com/ts/Student.java
+++ Laskpur-hibernate-1/src/main/java/com/ts/Student.java
@@ -1,15 +1,39 @@
 package com.ts;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import java.util.Date;
 
-@Entity
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity(name = "std")
 public class Student {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int rollno;
+	
+	@Column(name = "student_name")
 	private String name;
+	
+	@Column(name= "city")
 	private String address;
+	
+	@Temporal(TemporalType.TIME)
+	private Date dob;
+	
+	
+	
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
 	
 	
 	public int getRollno() {
@@ -39,5 +63,15 @@ public class Student {
 		this.address = address;
 	}
 	
+	public Student(String address, int rollno, String name) {
+		this.rollno = rollno;
+		this.name = name;
+		this.address = address;
+	}
 	
+	public Student(String name, String address, Date dob) {
+		this.name = name;
+		this.address = address;
+		this.dob = dob;
+	}
 }
