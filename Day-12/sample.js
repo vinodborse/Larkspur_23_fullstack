0b44eb36056d9bf9f0c13bb697bbcb06e40079bb
Vinodkumar Borse <borse002@gmail.com>
2024-01-16 09:40:05 +0530

    Add files via upload

--- Day-12/sample.js
+++ Day-12/sample.js
@@ -3,13 +3,13 @@ async function dataSearch() {
     
     const val = document.getElementById('inp').value;
 
-    const url = "https://dummyjson.com/products/"+val;
+    const url = "http://localhost:8080/get-student?rollno="+val;
 
     const data = await fetch(url, {method: 'GET'});
     console.log("MY DATA = ", data);
     const finalData = await data.json();
 
-    document.getElementById('dt').innerHTML = "<h1 style='color: red'> Title:- " + finalData.title +"</h1> <h1 style='color: green'> Price:- $" + finalData.price +"</h1>";
+    document.getElementById('dt').innerHTML = "<h1 style='color: red'> Name:- " + finalData.name +"</h1> <h1 style='color: green'> Address:- " + finalData.address +"</h1>";
 
 }
 
